0d297fa7d70588b7d433bf9a4dc1122288d96ccc
loic28130 <loicgiraudet@gmail.com>
2022-03-11 18:50:52 +0100

    en-tete et pied de page

---
+++ structure/en-tete.php
@@ -0,0 +1,19 @@
+
+
+
+<div id="en-tete">
+    <div id="titre">
+        <img src="image/logo.png" alt="">
+        <h1>chauffeur/livreur privée</h1>
+    </div> 
+
+    <div id="bouton">
+        <a href="index.html" class="bouton">acceuill</a>
+        <a href="chauffeur.html" class="bouton">chauffeur</a>
+        <a href="livraison.html" class="bouton">livraison colis</a>
+    </div>
+</div> 
+
+
+
+
