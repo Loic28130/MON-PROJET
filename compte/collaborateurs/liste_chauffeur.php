f92dce62eb73bf9942ce780cbd49c02374b2cb67
loic28130 <loicgiraudet@gmail.com>
2022-12-03 17:55:39 +0100

    tableau RDV chauffeur

---
+++ compte/collaborateurs/liste_chauffeur.php
@@ -0,0 +1,60 @@
+
+        <section id="Contenu">
+
+<!-- tableau d'affichage de $result -->
+    <table class=table>
+        <thead>
+            <tr class=table1>
+                <td>lieux de départ</td>
+                <td>adresse d'arrivée</td>
+                <td>date de départ</td>
+                <td>heure de départ</td>
+                
+            </tr>
+        </thead>
+        <tbody>
+            <?php $connect = connectionBDD();
+
+                $SelectID=$_SESSION["ID"];
+                $requete="SELECT * FROM `rdv_chauffeur` WHERE `ID_collaborateurs` ='". $SelectID."'" ;
+
+                if ($result=mysqli_query ($connect,$requete)) {
+
+                    // fetch_assoc=recuperée les valeur dans un tableau associatif
+                    $row = mysqli_fetch_assoc($result);
+                    if ($row==true){ ?>
+                    <tr class=table2>
+                        <td><?php echo $row["lieux de depart"]; ?></td>
+
+                        <td><?php echo $row["adresse arrivee"]; ?></td>
+
+                        <td><?php echo $row["date de depart"]; ?></td>
+
+                        <td><?php echo  $row["heure de depart"]; ?></td>
+                    </tr>
+
+
+                    <!-- <div class="modif">
+                    <a href="index.php?page=F_updatePrenom"<?php echo $row["IDclient"]; ?>>modifier</a></td> 
+                    <a href="index.php?page=F_updateNom"<?php echo $row["IDclient"]; ?>>modifier</a></td>
+                    <a href="index.php?page=F_updateEmail"<?php echo $row["IDclient"]; ?>>modifier</a></td>
+                    <a href="index.php?page=F_updateMotDePasse"<?php echo $row["IDclient"]; ?>>modifier</a></td> 
+                    </div> -->
+            
+            <?php
+                    }   
+                    else {
+                        echo "vide";
+                        }
+                }
+
+                   
+            
+            mysqli_close($connect);
+            ?>
+        </tbody>
+    </table>
+
+</section>
+
+
