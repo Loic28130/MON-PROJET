448fdb6c8bdba9f4ac3015cf5819ad317628cb8c
loic28130 <loicgiraudet@gmail.com>
2023-03-28 19:49:28 +0200

    requete preparée chauffeur et livreur  (.php)

--- chauffeur.php
+++ chauffeur.php
@@ -7,11 +7,19 @@ $saisie_adresse_arrivee=$_POST ["adresse_arrivee"];
 $saisie_date_de_depart=$_POST ["date_de_depart"];
 $saisie_heure_de_depart=$_POST ["heure_de_depart"];
 $SaisieId=$_POST ["ID"];
-$requete= "INSERT INTO `rdv_chauffeur`( adresse_de_depart, adresse_arrivee, `date_de_depart`, `heure_de_depart`, `ID_clients`)  VALUES ('$saisie_lieux_de_depart','$saisie_adresse_arrivee','$saisie_date_de_depart','$saisie_heure_de_depart','$SaisieId')";
-if (mysqli_query($connect, $requete)) {
+$requete= "INSERT INTO `rdv_chauffeur`( adresse_de_depart, adresse_arrivee, `date_de_depart`, `heure_de_depart`, `ID_clients`)  VALUES (?, ?, ?, ?, ?)";
+
+if  ($requetePrepare = mysqli_prepare($connect, $requete)){
+  // bind mes valeur avec les ?
+  mysqli_stmt_bind_param($requetePrepare, "sssss", $saisie_lieux_de_depart, $saisie_adresse_arrivee, $saisie_date_de_depart, $saisie_heure_de_depart, $SaisieId);
+  // execution de la requete prepare
+  mysqli_stmt_execute($requetePrepare);
+
     $_SESSION['success'] = 'RDV enregistré';
     header ("location:index.php?page=index");
-  } else {
+  } 
+  
+  else {
     echo "Erreur: " . $requete . "<br>" . mysqli_error($connect);
     exit();
   }
--- livreur.php
+++ livreur.php
@@ -12,8 +12,14 @@ $saisie_recuperation=$_POST ["recuperation"];
 $saisie_destination=$_POST ["destination"];
 $saisie_date_de_recuperation=$_POST ["date_de_recuperation"];
 $SaisieId=$_POST ["ID"];
-$requete= "INSERT INTO `rdv_livreur`( `adresse_de_recuperation`, `adresse_de_livraison`, `date_de_livraison`, `ID_clients`) VALUES ('$saisie_recuperation','$saisie_destination','$saisie_date_de_recuperation','$SaisieId')";
-if (mysqli_query($connect, $requete)) {
+$requete= "INSERT INTO `rdv_livreur`( `adresse_de_recuperation`, `adresse_de_livraison`, `date_de_livraison`, `ID_clients`) VALUES (?, ?, ?, ?)";
+
+if  ($requetePrepare = mysqli_prepare($connect, $requete)){
+  // bind mes valeur avec les ?
+  mysqli_stmt_bind_param($requetePrepare, "ssss", $saisie_recuperation, $saisie_destination, $saisie_date_de_recuperation, $SaisieId);
+  // execution de la requete prepare
+  mysqli_stmt_execute($requetePrepare);
+
   $_SESSION['success'] = 'RDV enregistré';
   header ("location:index.php?page=index");
   } else {
