ca787704c11903d634c7bf2ab16e2759d3645fc7
loic28130 <loicgiraudet@gmail.com>
2023-03-28 20:29:09 +0200

    requete préparée inscrire

--- inscrire.php
+++ inscrire.php
@@ -9,12 +9,19 @@ $saisie_prenom=$_POST ["prenom"];
 $saisie_nom=$_POST ["nom"];
 $saisie_email=$_POST ["email"];
 $saisie_MotDePasse=hachage( $_POST ["mot_de_passe"]);
-$requete= "INSERT INTO `clients`( `nom`, `prenom`, `email`, `mot_de_passe`) VALUES ('$saisie_nom','$saisie_prenom','$saisie_email','$saisie_MotDePasse')";
-if (mysqli_query($connect, $requete)) {
+$requete= "INSERT INTO `clients`( `nom`, `prenom`, `email`, `mot_de_passe`) VALUES (?, ?, ?, ?)";
+
+if ($requetePrepare = mysqli_prepare($connect, $requete)){
+  // bind mes valeur avec les ?
+  mysqli_stmt_bind_param($requetePrepare, "ssss", $saisie_nom, $saisie_prenom, $saisie_email, $saisie_MotDePasse);
+  // execution de la requete prepare
+  mysqli_stmt_execute($requetePrepare);
     // echo "compte créer";
     $_SESSION['success'] = 'compte créer vous pouvez vous connectée';
     header ("location:index.php?page=connection&type=client");
-  } else {
+  } 
+  
+  else {
     echo "Erreur: " . $requete . "<br>" . mysqli_error($connect);
   }
 ?>
