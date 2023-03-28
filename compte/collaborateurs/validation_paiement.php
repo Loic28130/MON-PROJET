0693dbaebf1b6405e1c73e7034d99af8f57416e5
loic28130 <loicgiraudet@gmail.com>
2023-03-28 20:18:46 +0200

    requéte préparée validation paiement

--- compte/collaborateurs/validation_paiement.php
+++ compte/collaborateurs/validation_paiement.php
@@ -9,9 +9,14 @@ if( $Rdv == "chauffeur"){
     $saisie_dateDeDepart=$_POST ["date"];
     $saisie_prix=$_POST ["prix"];
     $saisie_ID=$_POST ["ID"];
-    $requete= "INSERT INTO `paiement`( `nom`, `prenom`, `date`, `prix`, `ID_RDV_chauffeur`) VALUES ('$saisie_nom','$saisie_prenom','$saisie_dateDeDepart','$saisie_prix','$saisie_ID')";
+    $requete= "INSERT INTO `paiement`( `nom`, `prenom`, `date`, `prix`, `ID_RDV_chauffeur`) VALUES (?, ?, ?, ?, ?)";
 
-    if (mysqli_query($connect, $requete)) {
+    if ($requetePrepare = mysqli_prepare($connect, $requete)){
+      // bind mes valeur avec les ?
+      mysqli_stmt_bind_param($requetePrepare, "sssss", $saisie_nom, $saisie_prenom, $saisie_dateDeDepart, $saisie_prix, $saisie_ID);
+      // execution de la requete prepare
+      mysqli_stmt_execute($requetePrepare);
+      
     $_SESSION['success'] = 'paiement valider';
     header ("location:index.php?page=liste_chauffeur&IDrdvChauffeur");
     } else {
@@ -26,12 +31,19 @@ if( $Rdv == "chauffeur"){
     $saisie_dateDeDepart=$_POST ["date"];
     $saisie_prix=$_POST ["prix"];
     $saisie_ID=$_POST ["ID"];
-    $requete= "INSERT INTO `paiement`( `nom`, `prenom`, `date`, `prix`, `ID_RDV_livreur`) VALUES ('$saisie_nom','$saisie_prenom','$saisie_dateDeDepart','$saisie_prix','$saisie_ID')";
+    $requete= "INSERT INTO `paiement`( `nom`, `prenom`, `date`, `prix`, `ID_RDV_livreur`) VALUES (?, ?, ?, ?, ?)";
 
-      if (mysqli_query($connect, $requete)) {
+      if ($requetePrepare = mysqli_prepare($connect, $requete)){
+        // bind mes valeur avec les ?
+        mysqli_stmt_bind_param($requetePrepare, "sssss", $saisie_nom, $saisie_prenom, $saisie_dateDeDepart, $saisie_prix, $saisie_ID);
+        // execution de la requete prepare
+        mysqli_stmt_execute($requetePrepare);
+
       $_SESSION['success'] = 'paiement valider';
       header ("location:index.php?page=liste_livreur&IDrdvlivreur");
-      } else {
+      } 
+      
+      else {
         echo "Erreur: " . $requete . "<br>" . mysqli_error($connect);
         exit();
       }
