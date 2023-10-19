227b2600232019c7c0e7fddcec026db5a01a65d2
loic28130 <loicgiraudet@gmail.com>
2023-10-19 11:35:34 +0200

    liste clients et debut liste collaborateurs

---
+++ compte/admin/liste_clients(admin).php
@@ -0,0 +1,51 @@
+
+
+<section id="Contenu">
+
+    <!-- tableau d'affichage de $result -->
+    <table class=liste>
+        <thead>
+            <tr class=liste1>
+                <td>Nom</td>
+                <td>Prenom</td>
+                <td>adresse mail</td>
+            </tr>
+        </thead>
+        
+        <tbody>
+            <?php $connect = connectionBDD();
+            // var_dump()
+                // $SelectID=$_SESSION["ID"];
+                $requete="SELECT nom , prenom , email FROM clients";
+
+                if($requetePrepare = mysqli_prepare($connect, $requete)){
+                    
+                    // mysqli_stmt_bind_param($requetePrepare,);
+                    
+                    mysqli_stmt_execute($requetePrepare);
+                     
+                    mysqli_stmt_bind_result($requetePrepare, $nom, $prenom, $adresseMail);
+                    
+                   while (mysqli_stmt_fetch($requetePrepare)){
+                    ?>
+                    <tr class=liste2>
+                            <td><?php echo $nom; ?></td>
+
+                            <td><?php echo $prenom; ?></td>
+
+                            <td><?php echo $adresseMail; ?></td>
+                        </tr>
+                       <?php
+                   };
+
+                   mysqli_stmt_close($requetePrepare);
+                } 
+            
+            mysqli_close($connect);
+            ?>
+        </tbody>
+    </table>
+
+</section>
+
+
