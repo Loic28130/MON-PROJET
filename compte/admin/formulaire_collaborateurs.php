4395c5fb43843d457a0241203cabe61d439cbcbc
loic28130 <loicgiraudet@gmail.com>
2023-11-22 10:16:28 +0100

    update reussi

--- compte/admin/formulaire_collaborateurs.php
+++ compte/admin/formulaire_collaborateurs.php
@@ -4,11 +4,11 @@
     $connect = connectionBDD();
 
     $IDcollaborateurs=$_GET ["IDcollaborateurs"];
-    $requete="SELECT nom , prenom , email  FROM `collaborateurs` WHERE ID_collaborateurs = ?" ;
+    $requete="SELECT nom , prenom , email  FROM `collaborateurs` WHERE ID_collaborateurs = $IDcollaborateurs" ;
 
     if($requetePrepare = mysqli_prepare($connect, $requete)){
         
-        mysqli_stmt_bind_param($requetePrepare, "s" , $IDcollaborateurs);
+        // mysqli_stmt_bind_param($requetePrepare, $IDcollaborateurs);
         
         mysqli_stmt_execute($requetePrepare);
        
@@ -16,7 +16,7 @@
         
         mysqli_stmt_fetch($requetePrepare);?>
 
-<form action="index.php?page=modif_collaborateur(admin)&IDcollaborateurs=<?php echo $ID_collaborateurs?>" method="post" class="formulaire">
+<form action="index.php?page=modif_collaborateur(admin)" method="post" class="formulaire">
     <label for="nom">nom</label>
     <input type="text" value="<?php echo $nom; ?>" name="nom" required autofocus>
 
@@ -24,11 +24,13 @@
     <input type="text" value="<?php echo $prenom; ?>" name="prenom" required autofocus>
 
     <label for="email">email</label>
-    <input type="text" value="<?php echo $email; ?>" name="email" required autofocus> 
+    <input type="text" value="<?php echo $email; ?>" name="email" required autofocus>
+    
+    <input type="hidden" value="<?php echo $IDcollaborateurs; ?>" name="IDcollaborateurs" >
 
     <center><button class="bouton">modifier</button></center>
 </form>
-
+<?php var_dump($IDcollaborateurs)?>
 <?php
     }
 mysqli_close($connect);
--- compte/admin/modif_collaborateurs.php
+++ compte/admin/modif_collaborateurs.php
@@ -4,20 +4,20 @@
 
   $connect = connectionBDD();
 
-  $IDcollaborateurs=$_GET ["IDcollaborateurs"];
+  $IDcollaborateurs=$_POST ["IDcollaborateurs"];
   $saisie_nom=$_POST ["nom"];
   $saisie_prenom=$_POST ["prenom"];
   $saisie_email=$_POST ["email"];
-  $requete= "UPDATE `collaborateurs` SET `nom`='".$saisie_nom."' `prenom`='".$saisie_prenom."' `email`='".$saisie_email."' WHERE  `ID_collaborateurs`= ?";
+  $requete= "UPDATE `collaborateurs` SET `nom`= '".$saisie_nom."', `prenom`= '".$saisie_prenom."', `email`= '".$saisie_email."' WHERE  `ID_collaborateurs`= '".$IDcollaborateurs."'";
 
   if ($requetePrepare = mysqli_prepare($connect, $requete)){
    
-    // mysqli_stmt_bind_param($requetePrepare, "s", $IDcollaborateurs);
+    // mysqli_stmt_bind_param($requetePrepare, "?");
     
     mysqli_stmt_execute($requetePrepare);
 
     $_SESSION['success'] = 'modification réussi';
-    // $_SESSION["nom"] = $saisie_nom;
+    
     header ("location:index.php?page=liste_collaborateurs_admin");
     } 
     
